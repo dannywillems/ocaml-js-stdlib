8ac20ad6aac0b312b5e5785ebdbd1aef7d18ad63
Danny Willems <contact@danny-willems.be>
2016-03-26 18:24:56 +0100

    add readme and date binding. Not tested

---
+++ README.md
@@ -0,0 +1,30 @@
+Binding to Javascript standard library in OCaml
+===============================================
+
+Some technologies such as [js_of_ocaml](https://ocsigen.com/js_of_ocaml)
+provides a compiler from OCaml to javascript allowing ocaml developer to use
+OCaml to develop such as applications running in a browser or mobile
+applications using cordova.
+
+This library aims to provide a binding, using *pure* OCaml type (not using JS
+module from js_of_ocaml for example).
+
+## How is this library implemented
+
+We try to use [gen_js_api](https://github.com/lexifi/gen_js_api) which aims to
+provide binding to javascript independently of the ocaml to javascript compiler.
+With gen_js_api, you only need to provide the interface (mli file) and
+gen_js_api executable outputs the implementation.
+
+## How to contribute
+
+This library is on a very early stage of development. It is developed when we
+need javascript functions provided in the standard library. Do not hesitate to
+contribute.
+
+## Maintainers
+
+Danny Willems
+* Twitter: @dwillems42
+* Github: https://github.com/dannywillems
+* Email: contact@danny-willems.be
---
+++ date.mli
@@ -0,0 +1,91 @@
+class date : Ojs.t ->
+  object
+    inherit Ojs.obj
+
+    method get_date                 : int
+    method get_day                  : int
+    method get_full_year            : int
+    method get_hours                : int
+    method get_milliseconds         : int
+    method get_minutes              : int
+    method get_month                : int
+    method get_secondes             : int
+    method get_time                 : int
+    method get_timezone_offset      : int
+    method get_UTC_date             : int
+    method get_UTC_day              : int
+    method get_UTC_full_year        : int
+    method get_UTC_hours            : int
+    method get_UTC_milliseconds     : int
+    method get_UTC_minutes          : int
+    method get_UTC_month            : int
+    method get_UTC_seconds          : int
+    (* DEPRECATED *)
+    (* method get_year                 : int *)
+
+    method now                      : int
+    method parse                    : string -> int
+
+    method set_date                 : int -> unit
+    [@@js.set "setDate"]
+    method set_day                  : int -> unit
+    [@@js.set "setDay"]
+    method set_full_year            : int -> unit
+    [@@js.set "setFullYear"]
+    method set_hours                : int -> unit
+    [@@js.set "setHours"]
+    method set_milliseconds         : int -> unit
+    [@@js.set "setMilliseconds"]
+    method set_minutes              : int -> unit
+    [@@js.set "setMinutes"]
+    method set_month                : int -> unit
+    [@@js.set "setMonth"]
+    method set_secondes             : int -> unit
+    [@@js.set "setSecondes"]
+    method set_time                 : int -> unit
+    [@@js.set "setTime"]
+    method set_timezone_offset      : int -> unit
+    [@@js.set "setTimezoneOffset"]
+    method set_UTC_date             : int -> unit
+    [@@js.set "setUTCDate"]
+    method set_UTC_day              : int -> unit
+    [@@js.set "setUTCDay"]
+    method set_UTC_full_year        : int -> unit
+    [@@js.set "setUTCFullYear"]
+    method set_UTC_hours            : int -> unit
+    [@@js.set "setUTCHours"]
+    method set_UTC_milliseconds     : int -> unit
+    [@@js.set "setUTCMilliseconds"]
+    method set_UTC_minutes          : int -> unit
+    [@@js.set "setUTCMinutes"]
+    method set_UTC_month            : int -> unit
+    [@@js.set "setUTCMonth"]
+    method set_UTC_seconds          : int -> unit
+    [@@js.set "setUTCSeconds"]
+    (* DEPRECATED *)
+    (* method set_year                 : int -> unit *)
+
+    method to_date_string           : string
+    method to_ISO_string            : string
+    method to_JSON                  : string
+    method to_locale_date_string    : string
+    method to_locale_time_string    : string
+    method to_locale_string         : string
+    method to_string                : string
+    method to_time_string           : string
+    method to_UTC_string            : string
+    method utc                      : int
+    [@@js.call "UTC"]
+    method value_of                 : int
+  end
+
+val date                            : unit      -> date
+[@@js.new "Date"]
+val date_milli                      : int       -> date
+[@@js.new "Date"]
+val date_str                        : string    -> date
+[@@js.new "Date"]
+val date_full                       : int -> int -> int -> int -> int -> int ->
+                                      int -> date
+[@@js.new "Date"]
+

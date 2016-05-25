1c9b8a68b380b4784c11570f77847661542e3f13
Danny Willems <contact@danny-willems.be>
2016-05-25 21:02:32 +0200

    Use js_date from Lexifi

--- js_date.mli
+++ js_date.mli
@@ -1,93 +1,72 @@
-class date : Ojs.t ->
-  object
-    inherit Ojs.obj
+(* -------------------------------------------------------------------------- *)
+(**
+ * binding to Javascript date object.
+ *
+ * contributors:
+   * lexifi sas - https://www.lexifi.com/
+ *)
+(* -------------------------------------------------------------------------- *)
 
-    method get_date                 : int
-    method get_day                  : int
-    method get_full_year            : int
-    method get_hours                : int
-    method get_milliseconds         : int
-    method get_minutes              : int
-    method get_month                : int
-    method get_secondes             : int
-    method get_time                 : int
-    method get_timezone_offset      : int
-    method get_UTC_date             : int
-    method get_UTC_day              : int
-    method get_UTC_full_year        : int
-    method get_UTC_hours            : int
-    method get_UTC_milliseconds     : int
-    method get_UTC_minutes          : int
-    method get_UTC_month            : int
-    method get_UTC_seconds          : int
-    (* DEPRECATED *)
-    (* method get_year                 : int *)
+(** JS dates *)
 
-    method now                      : int
-    method parse                    : string -> int
+(** {2 Type definitions} *)
 
-    method set_date                 : int -> unit
-    [@@js.set "setDate"]
-    method set_day                  : int -> unit
-    [@@js.set "setDay"]
-    method set_full_year            : int -> unit
-    [@@js.set "setFullYear"]
-    method set_hours                : int -> unit
-    [@@js.set "setHours"]
-    method set_milliseconds         : int -> unit
-    [@@js.set "setMilliseconds"]
-    method set_minutes              : int -> unit
-    [@@js.set "setMinutes"]
-    method set_month                : int -> unit
-    [@@js.set "setMonth"]
-    method set_secondes             : int -> unit
-    [@@js.set "setSecondes"]
-    method set_time                 : int -> unit
-    [@@js.set "setTime"]
-    method set_timezone_offset      : int -> unit
-    [@@js.set "setTimezoneOffset"]
-    method set_UTC_date             : int -> unit
-    [@@js.set "setUTCDate"]
-    method set_UTC_day              : int -> unit
-    [@@js.set "setUTCDay"]
-    method set_UTC_full_year        : int -> unit
-    [@@js.set "setUTCFullYear"]
-    method set_UTC_hours            : int -> unit
-    [@@js.set "setUTCHours"]
-    method set_UTC_milliseconds     : int -> unit
-    [@@js.set "setUTCMilliseconds"]
-    method set_UTC_minutes          : int -> unit
-    [@@js.set "setUTCMinutes"]
-    method set_UTC_month            : int -> unit
-    [@@js.set "setUTCMonth"]
-    method set_UTC_seconds          : int -> unit
-    [@@js.set "setUTCSeconds"]
-    (* DEPRECATED *)
-    (* method set_year                 : int -> unit *)
+type t = private Ojs.t
+val t_of_js: Ojs.t -> t
+val t_to_js: t -> Ojs.t
 
-    method to_date_string           : string
-    method to_ISO_string            : string
-    method to_JSON                  : string
-    method to_locale_date_string    : string
-    method to_locale_time_string    : string
-    method to_locale_string         : string
-    method to_string                : string
-    method to_time_string           : string
-    method to_UTC_string            : string
-    method utc                      : int
-    [@@js.call "UTC"]
-    method value_of                 : int
-  end
+val now: unit -> t [@@js.new "Date"]
+val from_milliseconds: float -> t [@@js.new "Date"]
+val from_string: string -> t [@@js.new "Date"]
 
-val date                            : unit      -> date
-[@@js.new "Date"]
-val date_milli                      : int       -> date
-[@@js.new "Date"]
-val date_str                        : string    -> date
-[@@js.new "Date"]
-val date_full                       : int -> int -> int -> int -> int -> int ->
-                                      int -> date
-[@@js.new "Date"]
+val create: year:int -> month:int -> ?day:(int [@js.default 1]) -> ?hours:(int [@js.default 0]) -> ?minutes:(int [@js.default 0]) -> ?seconds:(int [@js.default 0]) -> ?ms:(int [@js.default 0]) -> unit -> t [@@js.new "Date"]
 
-val date_to_js : date -> Ojs.t
-val date_of_js : Ojs.t -> date
+val get_UTC_date: t -> int [@@js.call]
+val get_UTC_day: t -> int [@@js.call]
+val get_UTC_full_year: t -> int [@@js.call]
+val get_UTC_hours: t -> int [@@js.call]
+val get_UTC_milliseconds: t -> int [@@js.call]
+val get_UTC_minutes: t -> int [@@js.call]
+val get_UTC_month: t -> int [@@js.call]
+val get_UTC_seconds: t -> int [@@js.call]
+
+val set_UTC_date: t -> int -> unit [@@js.call]
+val set_UTC_full_year: t -> int -> unit [@@js.call]
+val set_UTC_hours: t -> int -> unit [@@js.call]
+val set_UTC_milliseconds: t -> int -> unit [@@js.call]
+val set_UTC_minutes: t -> int -> unit [@@js.call]
+val set_UTC_month: t -> int -> unit [@@js.call]
+val set_UTC_seconds: t -> int -> unit [@@js.call]
+
+val get_date: t -> int [@@js.call]
+val get_day: t -> int [@@js.call]
+val get_full_year: t -> int [@@js.call]
+val get_hours: t -> int [@@js.call]
+val get_milliseconds: t -> int [@@js.call]
+val get_minutes: t -> int [@@js.call]
+val get_month: t -> int [@@js.call]
+val get_seconds: t -> int [@@js.call]
+
+val set_date: t -> int -> unit [@@js.call]
+val set_full_year: t -> int -> unit [@@js.call]
+val set_hours: t -> int -> unit [@@js.call]
+val set_milliseconds: t -> int -> unit [@@js.call]
+val set_minutes: t -> int -> unit [@@js.call]
+val set_month: t -> int -> unit [@@js.call]
+val set_seconds: t -> int -> unit [@@js.call]
+
+val get_time: t -> float [@@js.call]
+val set_time: t -> float -> unit [@@js.call]
+
+val get_timezone_offset: t -> int [@@js.call]
+
+val to_locale_date_string: t -> string [@@js.call]
+val to_locale_string: t -> string [@@js.call]
+val to_locale_time_string: t -> string [@@js.call]
+
+val to_date_string: t -> string [@@js.call]
+val to_time_string: t -> string [@@js.call]
+
+val to_UTC_string: t -> string [@@js.call]
+
+val to_string: t -> string [@@js.call]
